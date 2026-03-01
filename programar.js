05cc80eb68b055e7e15088c9c0d810f6b42897af
guilherme8p9 <guilhermesaraivabraga@gmail.com>
2026-03-01 02:42:13 -0300

    tor20:0.3

--- programar.js
+++ programar.js
@@ -3,5 +3,9 @@ function tutorial() {
 };
 
 function dark() {
-    document.body.style.backgroundColor = "black";
+    const fc = document.getElementById("corpo");
+    fc.style.backgroundColor = fc.style.backgroundColor.includes("rgb(27, 25, 25)") ? "rgb(255, 255, 255)" : "rgb(27, 25, 25)";
+    const img = document.getElementById("dkm");
+    img.src = img.src.includes("https://cdn3.iconfinder.com/data/icons/block/32/moon-512.png") ? "https://png.pngtree.com/png-clipart/20221229/original/pngtree-icon-of-sun-png-image_8823429.png" : "https://cdn3.iconfinder.com/data/icons/block/32/moon-512.png";
+    img.style.borderColor = img.style.borderColor.includes("yellow") ? "black": "yellow";
 }
